2faeb21b00cc15892401fa6ec262c2e103e8077d
Faraz Ahmad <farazahmad759@gmail.com>
2021-03-27 15:30:57 +0500

    added and setup debug node package

---
+++ src/config/debug.js
@@ -0,0 +1,3 @@
+import debug from "debug";
+debug.enable("2by4image:global");
+export const debug_2by4Image = debug("2by4image:global");
--- src/controllers/image-operations/get-2by4-image.js
+++ src/controllers/image-operations/get-2by4-image.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import sharp from "sharp";
 import sizeOf from "image-size";
-
+import { debug_2by4Image } from "./../../config/debug.js";
 let mainUrls = {
   "t-shirt": {
     white: "assets/2020/07/MK-WhiteTshirt-MockUp-Blank-1.png",
@@ -16,6 +16,7 @@ let mainUrls = {
 };
 
 export async function get2by4Image(req, res) {
+  debug_2by4Image("hello");
   ////////////////////////////////////////////////
   // default values
   ////////////////////////////////////////////////
